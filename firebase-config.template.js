7728b9d8377ff26e95b477c7ccaebe57aa00f4ae
Salvador Frisco <salvadorfrisco70@gmail.com>
2025-07-17 22:23:10 -0300

    first commit

---
+++ firebase-config.template.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_AUTH_DOMAIN",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_STORAGE_BUCKET",
+  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
+  appId: "SEU_APP_ID",
+  measurementId: "SEU_MEASUREMENT_ID",
+};
+
+firebase.initializeApp(firebaseConfig);
