f97776dae44e5c015b7b34cb7c99c2cfd30cda53
Salvador Frisco <salvadorfrisco70@gmail.com>
2025-07-18 21:50:51 -0300

    feat: add call api users

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"72f2b18bb094f92f62a3113a8075240ebb59af
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4164387492"
+    serviceWorkerVersion: "1729860052"
   }
 });
