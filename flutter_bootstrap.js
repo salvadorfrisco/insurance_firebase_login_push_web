d8aa70b0e6d5b17011cc5edeb2212beb53bea4df
Salvador Frisco <salvadorfrisco70@gmail.com>
2025-07-22 19:05:02 -0300

    fix: return builder useravatar

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "261429772"
+    serviceWorkerVersion: "1789583956"
   }
 });
